38ace7d82628aea6b38957bf2179dd7c9292e6b1
shafqat1841 <shafqat1841@gmail.com>
2026-02-28 03:26:00 +0500

    Refactor argument handling in main function to enforce argument limits and improve output

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
 use std::env;
 
 fn main() {
-    let args2: Vec<String> = env::args().collect();
-    println!("args2: {:?}", args2);
+    let args: Vec<String> = env::args().collect();
+
+    let args_length: usize = args.len();
+    println!("{} arguments passed",args_length.to_string());
+
+    if args_length > 3 {
+        println!("The limit of args is 2");
+        println!("Usage: cargo run <arg1> <arg2>");
+        return;
+    }
+
+    if args_length < 3 {
+        println!("Not enough arguments passed");
+        println!("Usage: cargo run <arg1> <arg2>");
+        return;
+    }
+
+    for args_items in &args {
+        println!("args: {}", args_items);
+    }
+    println!("args: {:?}", args);
 }
