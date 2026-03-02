1689a1c95753b673ae89ef0522c4bf9c5e3b9190
shafqat1841 <shafqat1841@gmail.com>
2026-03-03 02:50:05 +0500

    Add helper module with AppCommand enum and command handling functions

---
+++ src/helper_file.rs
@@ -0,0 +1,30 @@
+pub mod helper_mod {
+    use crate::UserInput;
+
+
+    pub enum AppCommand {
+        Uppercase(String),
+        Lowercase(String),
+        Reverse(String),
+    }
+    
+    pub fn get_command_to_use(user_input: &UserInput) -> Option<AppCommand> {
+        let mut command_to_use: Option<AppCommand> = None;
+        match user_input.command.as_str() {
+            "uppercase" => command_to_use = Some(AppCommand::Uppercase(user_input.text.clone())),
+            "lowercase" => command_to_use = Some(AppCommand::Lowercase(user_input.text.clone())),
+            "reverse" => command_to_use = Some(AppCommand::Reverse(user_input.text.clone())),
+            _ => {}
+        }
+        command_to_use
+    }
+    
+    pub fn validate_args(args: &Vec<String>) -> bool {
+        let args_length: usize = args.len();
+        if args_length != 3 {
+            false;
+        }
+        true
+    }
+}
+
