bfd635ec5c8a86417f34356c22f3a0edb504fe02
shafqat1841 <shafqat1841@gmail.com>
2026-03-04 02:43:45 +0500

    Refactor get_command_to_use function to accept Ownership of UserInput directly and simplify text handling

--- src/helper_file.rs
+++ src/helper_file.rs
@@ -9,11 +9,11 @@ pub mod helper_mod {
     }
     
     // pub fn get_command_to_use(user_input: &UserInput) -> Option<AppCommand> {
-    pub fn get_command_to_use(user_input: &UserInput) -> Result<AppCommand, &'static str> {
+    pub fn get_command_to_use(user_input: UserInput) -> Result<AppCommand, &'static str> {
         match user_input.command.as_str() {
-            "uppercase" => return Ok(AppCommand::Uppercase(user_input.text.clone())),
-            "lowercase" => return Ok(AppCommand::Lowercase(user_input.text.clone())),
-            "reverse" => return Ok(AppCommand::Reverse(user_input.text.clone())),
+            "uppercase" => return Ok(AppCommand::Uppercase(user_input.text)),
+            "lowercase" => return Ok(AppCommand::Lowercase(user_input.text)),
+            "reverse" => return Ok(AppCommand::Reverse(user_input.text)),
             _ => return Err("Not valid command"),
         }
     }
