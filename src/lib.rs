86a0bf0f0094273b49392d04b76b003f126b9073
shafqat1841 <shafqat1841@gmail.com>
2026-03-04 02:30:42 +0500

    Remove validate_args function from public exports for improved modularity

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,5 @@ mod user_input_file;
 mod helper_file;
 
 pub use crate::user_input_file::user_input_mod::UserInput;
-pub use crate::helper_file::helper_mod::{AppCommand, get_command_to_use, validate_args};
+pub use crate::helper_file::helper_mod::{AppCommand, get_command_to_use};
 
