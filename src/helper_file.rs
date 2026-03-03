44b64fb97aa78e7c75a8d21e9dcbdedc069fcfed
shafqat1841 <shafqat1841@gmail.com>
2026-03-04 02:45:42 +0500

    Refactor get_command_to_use function to remove unnecessary return statements for cleaner code

--- src/helper_file.rs
+++ src/helper_file.rs
@@ -1,21 +1,19 @@
 pub mod helper_mod {
     use crate::UserInput;
 
-
     pub enum AppCommand {
         Uppercase(String),
         Lowercase(String),
         Reverse(String),
     }
-    
+
     // pub fn get_command_to_use(user_input: &UserInput) -> Option<AppCommand> {
     pub fn get_command_to_use(user_input: UserInput) -> Result<AppCommand, &'static str> {
         match user_input.command.as_str() {
-            "uppercase" => return Ok(AppCommand::Uppercase(user_input.text)),
-            "lowercase" => return Ok(AppCommand::Lowercase(user_input.text)),
-            "reverse" => return Ok(AppCommand::Reverse(user_input.text)),
-            _ => return Err("Not valid command"),
+            "uppercase" => Ok(AppCommand::Uppercase(user_input.text)),
+            "lowercase" => Ok(AppCommand::Lowercase(user_input.text)),
+            "reverse" => Ok(AppCommand::Reverse(user_input.text)),
+            _ => Err("Not valid command"),
         }
     }
 }
-
