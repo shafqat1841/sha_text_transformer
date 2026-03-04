63add970864a45a190ac384f0f404a54cb1cd90c
shafqat1841 <shafqat1841@gmail.com>
2026-03-05 02:31:26 +0500

    Refactor run function to improve error handling and remove unnecessary process exits

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,20 @@
-use std::process;
-
 mod helper_file;
 mod user_input_file;
 
-pub use crate::helper_file::helper_mod::AppCommand;
-pub use crate::user_input_file::user_input_mod::UserInput;
+pub use crate::helper_file::AppCommand;
+pub use crate::user_input_file::UserInput;
 
-pub fn run(args: std::env::Args) {
+pub fn run(args: std::env::Args) -> Result<(), &'static str> {
     if args.len() != 3 {
-        eprintln!("Wrong number of arguments passed");
         eprintln!("Usage: cargo run <arg1: Text> <arg2: Command>");
-        process::exit(1);
+        return Err("Wrong number of arguments passed");
     }
 
-    let user_input: UserInput = UserInput::build(args).unwrap_or_else(|err| {
-        eprintln!("Error: {}", err);
-        process::exit(1);
-    });
+    let user_input: UserInput = UserInput::build(args)?;
 
-    let command_to_use: AppCommand =
-        AppCommand::get_command_to_use(user_input).unwrap_or_else(|err| {
-            eprintln!("Error: {}", err);
-            process::exit(1);
-        });
+    let command_to_use: AppCommand = AppCommand::get_command_to_use(user_input)?;
 
     println!("{}", command_to_use);
+
+    Ok(())
 }
