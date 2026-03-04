f470e501415c8279042f4f36303fb66eb3c86c50
shafqat1841 <shafqat1841@gmail.com>
2026-03-05 02:01:24 +0500

    Refactor main function to remove unused get_command_to_use import and call method directly on AppCommand

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::env::Args;
 use std::process;
 
-use text_transformer::{AppCommand, UserInput, get_command_to_use};
+use text_transformer::{AppCommand, UserInput};
 
 fn main() {
     let args: Args = env::args();
@@ -18,10 +18,11 @@ fn main() {
         process::exit(1);
     });
 
-    let command_to_use: AppCommand = get_command_to_use(user_input).unwrap_or_else(|err| {
-        eprintln!("Error: {}", err);
-        process::exit(1);
-    });
+    let command_to_use: AppCommand =
+        AppCommand::get_command_to_use(user_input).unwrap_or_else(|err| {
+            eprintln!("Error: {}", err);
+            process::exit(1);
+        });
 
-    println!("{}",command_to_use);
+    println!("{}", command_to_use);
 }
