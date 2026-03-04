89c7771120781cbe6811b054c01e8df6e5c00043
shafqat1841 <shafqat1841@gmail.com>
2026-03-05 02:01:14 +0500

    Reorder module imports and remove unused get_command_to_use from public exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-
-mod user_input_file;
 mod helper_file;
+mod user_input_file;
 
+pub use crate::helper_file::helper_mod::AppCommand;
 pub use crate::user_input_file::user_input_mod::UserInput;
-pub use crate::helper_file::helper_mod::{AppCommand, get_command_to_use};
-
