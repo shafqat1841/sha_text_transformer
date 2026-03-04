ad2a673db87e33d566cdac773c50edcb44eec50a
shafqat1841 <shafqat1841@gmail.com>
2026-03-05 02:31:17 +0500

    Refactor UserInput struct by removing unnecessary module declaration and cleaning up code formatting

--- src/user_input_file.rs
+++ src/user_input_file.rs
@@ -1,28 +1,27 @@
-pub mod user_input_mod {
-    use std::env::Args;
 
-    pub struct UserInput {
-        pub text: String,
-        pub command: String,
-    }
+use std::env::Args;
 
-    impl UserInput {
-        pub fn build(mut args: Args) -> Result<UserInput, &'static str> {
-            args.next();
-            let text = match args.next() {
-                Some(arg) => arg,
-                None => return Err("Didn't get the text query string"),
-            };
+pub struct UserInput {
+    pub text: String,
+    pub command: String,
+}
 
-            let command = match args.next() {
-                Some(arg) => arg,
-                None => return Err("Didn't get the command query string"),
-            };
+impl UserInput {
+    pub fn build(mut args: Args) -> Result<UserInput, &'static str> {
+        args.next();
+        let text = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get the text query string"),
+        };
 
-            Ok(UserInput {
-                text,
-                command: command.to_lowercase(),
-            })
-        }
+        let command = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get the command query string"),
+        };
+
+        Ok(UserInput {
+            text,
+            command: command.to_lowercase(),
+        })
     }
 }
