4dad9cc31d8182183a52fe81c81b0a791a426f1b
shafqat1841 <shafqat1841@gmail.com>
2026-03-05 02:06:10 +0500

    Refactor main function to delegate argument handling and command execution to run function

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,10 @@
 use std::env;
 use std::env::Args;
-use std::process;
 
-use text_transformer::{AppCommand, UserInput};
+use text_transformer::run;
 
 fn main() {
     let args: Args = env::args();
 
-    if args.len() != 3 {
-        eprintln!("Wrong number of arguments passed");
-        eprintln!("Usage: cargo run <arg1: Text> <arg2: Command>");
-        process::exit(1);
-    }
-
-    let user_input: UserInput = UserInput::build(args).unwrap_or_else(|err| {
-        eprintln!("Error: {}", err);
-        process::exit(1);
-    });
-
-    let command_to_use: AppCommand =
-        AppCommand::get_command_to_use(user_input).unwrap_or_else(|err| {
-            eprintln!("Error: {}", err);
-            process::exit(1);
-        });
-
-    println!("{}", command_to_use);
+    run(args);
 }
