b3cb16b726c1a6b40ab5623eca260efe7dca67d1
shafqat1841 <shafqat1841@gmail.com>
2026-03-05 02:30:58 +0500

    remove unwanted helper_mod mod and separate display and text transformation logic

--- src/helper_file.rs
+++ src/helper_file.rs
@@ -1,38 +1,46 @@
-pub mod helper_mod {
-    use core::fmt;
+use core::fmt;
 
-    use crate::UserInput;
+use crate::UserInput;
 
-    pub enum AppCommand {
-        Uppercase(String),
-        Lowercase(String),
-        Reverse(String),
+pub enum AppCommand {
+    Uppercase(String),
+    Lowercase(String),
+    Reverse(String),
+}
+
+impl fmt::Display for AppCommand {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            AppCommand::Uppercase(_) => write!(f, "uppercase: {}", self.execute()),
+            AppCommand::Lowercase(_) => write!(f, "lowercase: {}", self.execute()),
+            AppCommand::Reverse(_) => write!(f, "reverse: {}", self.execute()),
+        }
     }
+}
 
-    impl fmt::Display for AppCommand {
-        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-            match self {
-                AppCommand::Uppercase(text) => write!(f, "uppercase: {}", text.to_uppercase()),
-                AppCommand::Lowercase(text) => write!(f, "lowercase: {}", text.to_lowercase()),
-                AppCommand::Reverse(text) => {
-                    let chars = text.chars();
-                    let reversed_chars = chars.rev();
-                    let reversed_text: String = reversed_chars.collect();
-                    write!(f, "reverse: {}", reversed_text)
-                }
+impl AppCommand {
+    fn execute(&self) -> String {
+        match self {
+            AppCommand::Uppercase(text) => text.to_uppercase(),
+            AppCommand::Lowercase(text) => text.to_lowercase(),
+            AppCommand::Reverse(text) => {
+                let chars = text.chars();
+                let reversed_chars = chars.rev();
+                let reversed_text: String = reversed_chars.collect();
+                return reversed_text;
             }
         }
     }
+}
 
-    impl AppCommand {
-        // pub fn get_command_to_use(user_input: &UserInput) -> Option<AppCommand> {
-        pub fn get_command_to_use(user_input: UserInput) -> Result<AppCommand, &'static str> {
-            match user_input.command.as_str() {
-                "uppercase" => Ok(AppCommand::Uppercase(user_input.text)),
-                "lowercase" => Ok(AppCommand::Lowercase(user_input.text)),
-                "reverse" => Ok(AppCommand::Reverse(user_input.text)),
-                _ => Err("Not valid command"),
-            }
+impl AppCommand {
+    // pub fn get_command_to_use(user_input: &UserInput) -> Option<AppCommand> {
+    pub fn get_command_to_use(user_input: UserInput) -> Result<AppCommand, &'static str> {
+        match user_input.command.as_str() {
+            "uppercase" => Ok(AppCommand::Uppercase(user_input.text)),
+            "lowercase" => Ok(AppCommand::Lowercase(user_input.text)),
+            "reverse" => Ok(AppCommand::Reverse(user_input.text)),
+            _ => Err("Not valid command"),
         }
     }
 }
