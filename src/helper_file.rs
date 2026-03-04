3f24951329ab3a9fee66f4fb7f493d44316a13bc
shafqat1841 <shafqat1841@gmail.com>
2026-03-05 02:01:04 +0500

    Make get_command_to_use function a trait of AppCommand

--- src/helper_file.rs
+++ src/helper_file.rs
@@ -24,13 +24,15 @@ pub mod helper_mod {
         }
     }
 
-    // pub fn get_command_to_use(user_input: &UserInput) -> Option<AppCommand> {
-    pub fn get_command_to_use(user_input: UserInput) -> Result<AppCommand, &'static str> {
-        match user_input.command.as_str() {
-            "uppercase" => Ok(AppCommand::Uppercase(user_input.text)),
-            "lowercase" => Ok(AppCommand::Lowercase(user_input.text)),
-            "reverse" => Ok(AppCommand::Reverse(user_input.text)),
-            _ => Err("Not valid command"),
+    impl AppCommand {
+        // pub fn get_command_to_use(user_input: &UserInput) -> Option<AppCommand> {
+        pub fn get_command_to_use(user_input: UserInput) -> Result<AppCommand, &'static str> {
+            match user_input.command.as_str() {
+                "uppercase" => Ok(AppCommand::Uppercase(user_input.text)),
+                "lowercase" => Ok(AppCommand::Lowercase(user_input.text)),
+                "reverse" => Ok(AppCommand::Reverse(user_input.text)),
+                _ => Err("Not valid command"),
+            }
         }
     }
 }
