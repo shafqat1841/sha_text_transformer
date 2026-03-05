f88d0948e3235c7ddaa9bab25196d49c8018acaa
shafqat1841 <shafqat1841@gmail.com>
2026-03-06 02:40:36 +0500

    Fix argument passing in main function to use mutable reference for Args

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ use std::env::Args;
 use text_transformer::run;
 
 fn main() {
-    let args: Args = env::args();
+    let mut args: Args = env::args();
 
-    if let Err(err) = run(args) {
+    if let Err(err) = run(&mut args) {
         eprintln!("Error: {}", err);
         std::process::exit(1);
     }
